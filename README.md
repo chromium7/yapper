36cdc4100cdb3b35f8433da9ec4220387f53665e
Chromium7 <christofersaputra29@gmail.com>
2020-08-23 07:39:42 +0700

    Updated tests.py and modified bootstrap style

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Yapper
+
+## CS50W Project - Network
+Design a Twitter-like social network website for making posts and following users.
+
+--------
+
+# **Yapper**
+
+### Yapper is a twitter-like social media created for those who just can't shut their *yappers*. 
+
+> Share your stories, gossips and yappies in Yapper!
+
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.contrib import auth
+from .models import *
 
-# Create your tests here.
+
+# Testing login form
+class AccountTestCase(TestCase):
+
+    def setUp(self):
+        # Create an account
+        self.user = User.objects.create_user(username="mjordan",
+                                             email="mjordan@example.com", password="mjordan123")
+
+    def test_register_account(self):
+        response = self.client.post('/register', {
+            'username': 'jdoe',
+            'email': 'jdoe@example.com',
+            'password': 'jdoe123',
+            'confirmation': 'jdoe123'
+        }, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_register_duplicate_account(self):
+        response = self.client.post('/register', {
+            'username': 'mjordan',
+            'email': 'mjordan@example.com',
+            'password': 'mjordan123',
+            'confirmation': 'mjordan123'
+        })
+        self.assertFalse(response.context['user'].is_authenticated)
+
+    def test_login_success(self):
+        response = self.client.post('/login', {
+            'username': 'mjordan',
+            'password': 'mjordan123'
+        }, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_login_fail(self):
+        response = self.client.post('/login', {
+            'username': 'mjordan123',
+            'password': 'mjordan'
+        })
+        self.assertFalse(response.context['user'].is_authenticated)
+
+# Test
+
+
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        pass
