00a6b47fb3056b99c9494dd285db448ac83fa799
Chromium7 <christofersaputra29@gmail.com>
2020-09-02 19:56:03 +0700

    Add ModelTestCase to tests.py

--- network/tests.py
+++ network/tests.py
@@ -44,23 +44,66 @@ class AccountTestCase(TestCase):
         })
         self.assertFalse(response.context['user'].is_authenticated)
 
-# Test
 
+# Testing models
 @tag('model')
 class ModelTestCase(TestCase):
 
     def setUp(self):
-        pass
+        self.user = User.objects.create_user(
+            username="mjordan", email="mjordan@example.com", password="mjordan123")
+        self.johny = User.objects.create_user(
+            username="johny", email="johny@example.com", password="johny123")
+        self.tom = User.objects.create_user(
+            username="tom", email="tom@example.com", password="tom123")
 
-@tag('view')
-class ViewTestCase(TestCase):
+    def test_create_post(self):
+        new_post = Post.objects.create(user=self.user, text="Hello, world")
+        post_likes = new_post.like.all()
+        post_replies = new_post.replies.all()
+        self.assertIsInstance(new_post, Post)
+        self.assertEqual(len(post_likes), 0)
+        self.assertEqual(len(post_replies), 0)
 
-    def setUp(self):
-        pass
+    def test_create_reply(self):
+        new_post = Post.objects.create(user=self.user, text="Hello, world")
+        reply = Reply.objects.create(
+            user=self.johny, text="Reply", post=new_post)
+        self.assertIsInstance(reply, Reply)
 
-@tag('script')
-class ScriptTestCase(TestCase):
+    def test_like_post(self):
+        new_post = Post.objects.create(user=self.user, text="Hello, world")
+        new_post.like.add(self.user)
 
-    def setUp(self):
-        pass
-    
+        self.assertEqual(new_post.like.count(), 1)
+
+    def test_get_liked_post(self):
+        new_post = Post.objects.create(user=self.user, text="Hello, world")
+        new_post.like.add(self.user)
+
+        liked_posts = self.user.likes.all()
+        self.assertEqual(liked_posts[0], new_post)
+
+    def test_create_following(self):
+        following = Following.objects.create(user=self.user, follow=self.johny)
+        self.assertIsInstance(following, Following)
+
+    def test_get_user_followers(self):
+        new_following = Following.objects.create(
+            user=self.user, follow=self.johny)
+        self.assertEqual(self.johny.followers.count(), 1)
+
+    def test_get_user_followings(self):
+        new_following = Following.objects.create(
+            user=self.user, follow=self.johny)
+        self.assertEqual(self.user.followings.count(), 1)
+
+    def test_serialize(self):
+        serialized_obj = self.user.serialize()
+        self.assertEqual(serialized_obj['name'], self.user.username)
+        self.assertEqual(serialized_obj['email'], self.user.email)
+        self.assertEqual(serialized_obj['desc'], self.user.profile_desc)
+        self.assertEqual(
+            serialized_obj['followingCount'], self.user.followings.count())
+        self.assertEqual(
+            serialized_obj['followerCount'], self.user.followers.count())
